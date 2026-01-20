a93fd0ba3a281f2782d0aea3c51446c82d51d3ee
Karan Jamwal <karanjamwal2004@gmail.com>
2026-01-20 19:52:44 +0530

    chore: update and add project dependencies.

---
+++ React/react-hook-form/src/App.jsx
@@ -0,0 +1,55 @@
+import "./index.css";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline mb-4 flex justify-center">
+        React Hook Form
+      </h1>
+      <form
+        onSubmit={handleFormSubmit}
+        className="flex flex-col items-center justify-center gap-4"
+      >
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer hover:bg-blue-600 transition-colors duration-200"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default App;
