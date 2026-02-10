ae80efb5e800a309ab3a6bfb69adbd3621f68641
Karan Jamwal <karanjamwal2004@gmail.com>
2026-02-10 22:55:17 +0530

    feat: Add new React class-based and functional components.

---
+++ components/classBased.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClassBased extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a class based component</h1>
+      </div>
+    );
+  }
+}
+
+export default ClassBased;
---
+++ components/funcBased.jsx
@@ -0,0 +1,5 @@
+const funcBased = () => {
+  return <div>This is a function based component</div>;
+};
+
+export default funcBased;
